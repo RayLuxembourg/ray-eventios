f5cb5eb0d608e4393bb7d9422f05159900b672cc
Ray <ij.kortez@gmail.com>
2017-10-17 00:38:24 +0300

    update

--- src/views/Event/ducks/reducer.js
+++ src/views/Event/ducks/reducer.js
@@ -1,4 +1,5 @@
-import { Map, List } from "immutable";
+import { combineReducers } from "redux-immutable";
+import { Map } from "immutable";
 import {
   GET_EVENT_BY_ID,
   GET_EVENT_BY_ID_SUCCESS,
@@ -11,47 +12,31 @@ import {
   UNATTEND_EVENT_FAIL
 } from "./types";
 
-const initialState = Map({
-  loading: false,
-  error: null,
-  event: Map(),
-  attendees:Map()
-});
-const onStart = { loading: true, error: null };
-export default function reducer(state = initialState, action = {}) {
+
+const event = (state = Map(), action) => {
   switch (action.type) {
-    case GET_EVENT_BY_ID:
-      return state.merge(onStart);
     case GET_EVENT_BY_ID_SUCCESS:
-      return state.merge({
-        loading: "false",
-        event: Map(action.payload.result),
-        attendees: Map(action.payload.entities.attendees)
-      });
-    case GET_EVENT_BY_ID_FAIL:
-      return state.merge({ error: true, loading: false });
-
-    case ATTEND_EVENT:
-      return state.merge(onStart);
-
+      return Map(action.payload.result);
     case ATTEND_EVENT_SUCCESS:
     case UNATTEND_EVENT_SUCCESS:
-    console.log(action.payload);
-      return state.merge({
-        event:Map(action.payload.result),
-        attendees:Map(action.payload.entities.attendees),
-        loading:false
-      });
-    case ATTEND_EVENT_FAIL:
-      return state.merge({ error: true, loading: false });
-
-    case UNATTEND_EVENT:
-      return state.merge(onStart);
-
-
-    case UNATTEND_EVENT_FAIL:
-      return state.merge({ error: true, loading: false });
+      return Map(action.payload.result);
     default:
       return state;
   }
-}
+};
+const attendees = (state = Map(), action) => {
+  switch (action.type) {
+    case GET_EVENT_BY_ID_SUCCESS:
+      return Map(action.payload.entities.attendees);
+    case ATTEND_EVENT_SUCCESS:
+    case UNATTEND_EVENT_SUCCESS:
+      return Map(action.payload.entities.attendees);
+    default:
+      return state;
+  }
+};
+export default combineReducers({
+  event,
+  attendees
+})
+
